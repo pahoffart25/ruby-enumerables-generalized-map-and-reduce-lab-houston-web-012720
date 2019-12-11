5a2d55a83e4a3d2a99e848152b79ef6181bacdb8
Paul Hoffart <pahoffart25@gmail.com>
2019-12-11 20:53:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,16 +1,3 @@
-# Your Code Here
-#def my_own_map(array)
-#  return array.map { |e| e * -1 }
-#end
-
-#def map(array)
-#  return array.map { |e| e }
-#end
-
-#def map(array)
-#  return array.map { |n| n * 2 }
-#end
-
 def map(array)
   new = []
   i = 0
@@ -20,3 +7,4 @@ def map(array)
   end
   new
 end
+
